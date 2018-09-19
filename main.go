92f4b409833ee3ff9e008d078cdac77542f46ee6
Jon Jelinski <jon_jelinski@t-online.de>
2018-09-19 23:39:14 +0300

    Add basic go udp sender receiver

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	port := ":1337"
+	fmt.Println("Start listening on port " + port)
+
+	pc, err := net.ListenPacket("udp", port)
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	defer pc.Close()
+
+	for {
+		buf := make([]byte, 1024)
+		n, addr, err := pc.ReadFrom(buf)
+
+		if err != nil {
+			log.Print("Error: ", err)
+			continue
+		}
+
+		log.Println("Received: ", buf)
+
+		go serve(pc, addr, buf[:n])
+	}
+}
+
+func serve(pc net.PacketConn, addr net.Addr, buf []byte) {
+	buf[2] |= 0x80
+	pc.WriteTo(buf, addr)
+}
