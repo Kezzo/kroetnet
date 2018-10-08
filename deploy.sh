da938f5acc535fdc7d6cf498a6fd15586ab59d61
Jon Jelinski <jon_jelinski@t-online.de>
2018-10-08 23:22:37 +0300

    Update server IP in deploy and restart script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ docker build -t dev-trinity-container-registry .
 docker tag dev-trinity-container-registry:latest 524454272832.dkr.ecr.eu-west-1.amazonaws.com/dev-trinity-container-registry:latest
 docker push 524454272832.dkr.ecr.eu-west-1.amazonaws.com/dev-trinity-container-registry:latest
 
-ssh ec2-user@34.248.140.154 -i ~/.ssh/dev-trinity-key-pair.pem 'docker kill $(docker ps --format "{{.ID}}" --filter "name=dev-trinit");uname -a;exit'
+ssh ec2-user@34.254.60.219 -i ~/.ssh/dev-trinity-key-pair.pem 'docker kill $(docker ps --format "{{.ID}}" --filter "name=dev-trinit");uname -a;exit'
 
 aws ecs update-service --cluster dev-trinity-cluster --service dev-trinity-container-service --force-new-deployment
--- restart.sh
+++ restart.sh
@@ -1,1 +1,1 @@
-ssh ec2-user@34.248.140.154 -i ~/.ssh/dev-trinity-key-pair.pem 'docker restart $(docker ps --format "{{.ID}}" --filter "name=dev-trinit");uname -a;exit'
+ssh ec2-user@34.254.60.219 -i ~/.ssh/dev-trinity-key-pair.pem 'docker restart $(docker ps --format "{{.ID}}" --filter "name=dev-trinit");uname -a;exit'
